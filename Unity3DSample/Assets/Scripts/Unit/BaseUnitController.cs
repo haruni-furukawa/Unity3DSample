ba717800a4261b8fe261bef711ec63fecd7cf33d
HaruniFurukawa <furukawa@haru-ni.net>
2023-06-19 17:45:47 +0900

    [fix] Unitの移動方法を実装

---
+++ Unity3DSample/Assets/Scripts/Unit/BaseUnitController.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseUnitController : MonoBehaviour
+{
+    // ---------- 定数宣言 ----------
+    // ---------- プレハブ ----------
+    // ---------- ゲームオブジェクト参照変数宣言 ----------
+    [SerializeField] private Transform _cameraTransform; // カメラのTransformを参照するための変数
+    [SerializeField] private Rigidbody _rigidBody; // 剛体
+    // ---------- プロパティ ----------
+    [SerializeField] private float _rotateSpeed = 1000f; // キャラクターの回転速度
+    [SerializeField] private float _moveSpeed = 7.0f; // 移動速度
+    [SerializeField] private float _jumpPower = 6.0f; // ジャンプ威力
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private float _groundDistance = 0.1f;
+    private bool _isJumping;
+    private bool _isGrounded;
+    // ---------- クラス変数宣言 ----------
+    // ---------- インスタンス変数宣言 ----------
+    private Vector3 _velocity;
+    // ---------- コンストラクタ・デストラクタ ----------
+    // ---------- Unity組込関数 ----------
+    private void Start()
+    {
+        Initialize();
+    }
+
+    private void Update()
+    {
+        // Inputから移動量を取得
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        // 移動量を保存
+        _velocity = _cameraTransform.TransformDirection(new Vector3(horizontal, 0, vertical));
+
+        // ジャンプ
+        if (Input.GetButtonDown("Jump"))
+        {
+            _isJumping = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // 重力下での移動処理
+        Move();
+
+        // キャラクターのレイヤーマスクを作成
+        int layerMask = ~LayerMask.GetMask("Player");
+
+        // レイキャストを使って地面との接触を検出
+        _isGrounded = Physics.CheckSphere(_groundCheck.position, _groundDistance, layerMask);
+        // ジャンプ処理
+        if (_isGrounded && _isJumping)
+        {
+            Jump();
+            _isJumping = false;
+        }
+    }
+    // ---------- Public関数 ----------
+    // ---------- Protected関数 ----------
+    // ---------- Private関数 ----------
+
+    // 初期化
+    protected virtual void Initialize()
+    {
+        return;
+    }
+
+    // 移動
+    protected virtual void Move()
+    {
+        if (_velocity.magnitude > 0)
+        {
+            if (_velocity.magnitude >= 0.15f)
+            {
+                // キャラクターの向きを移動方向に即座に変更
+                Quaternion lookRotation = Quaternion.LookRotation(_velocity);
+                // Y軸の回転のみを抽出
+                Quaternion targetRotation = Quaternion.Euler(0, lookRotation.eulerAngles.y, 0);
+
+                // 目的の向きへ徐々に近づく
+                Quaternion smoothedRotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotateSpeed * Time.deltaTime);
+
+                transform.rotation = smoothedRotation;
+            }
+
+            // 移動量に速度を掛ける
+            Vector3 moveVelocity = _velocity.normalized * _moveSpeed;
+
+            // y軸の速度はそのままにする（自由落下等の影響を受けるため）
+            moveVelocity.y = _rigidBody.velocity.y;
+
+            // Rigidbodyの速度に直接代入することで移動
+            _rigidBody.velocity = moveVelocity;
+        }
+        else
+        {
+            _rigidBody.velocity = new Vector3(0, _rigidBody.velocity.y, 0);
+        }
+    }
+
+    // ジャンプ処理
+    protected virtual void Jump()
+    {
+        // Rigidbodyに上方向への力を追加
+        _rigidBody.AddForce(new Vector3(0, _jumpPower, 0), ForceMode.VelocityChange);
+    }
+}
